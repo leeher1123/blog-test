363bf2b9b3197bcfe5f1582b45a09a8703238c61
leeher1123 <leeher1123@naver.com>
2021-10-24 12:57:13 +0900

    로그인, 회원가입 container 파일에 옮김

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React from "react";
 import styled from "styled-components";
 import {GlobalStyle} from "./style/GlobalStyle";
 import Header from "./views/shared/components/Header";
